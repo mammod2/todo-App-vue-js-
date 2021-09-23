b180312337a2c0afdc33716d5b5c497c85d0d457
mammod2 <mammod26@gmail.com>
2021-09-23 21:38:51 +0800

    first comit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue 3 Todo App</h1>
+  <form @submit.prevent="addNewTodo">
+    <label>New todo</label>
+    <input
+      v-model="newTodo"
+      type="text"
+      name="newTodo"
+      placeholder="Enter your tasks"
+    />
+    <button>Add New todo</button>
+  </form>
+
+  <button @click="markAllDone">All Done</button>
+  <button @click="removeAll">Remove all</button>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="todo.id">
+      <h3 :class="{ done: todo.done }" @click="toggleDone(todo)" class="todo">
+        {{ todo.content }}
+        <button @click="removeTodo(index)">Remove todo</button>
+      </h3>
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const newTodo = ref("");
+    const todos = ref([]);
+    function addNewTodo() {
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newTodo.value,
+      });
+      newTodo.value = "";
+    }
+    function toggleDone(todo) {
+      todo.done = !todo.done;
+    }
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+    }
+    function markAllDone() {
+      todos.value.forEach((todo) => (todo.done = true));
+    }
+    function removeAll() {
+      todos.value = [];
+    }
+    return {
+      addNewTodo,
+      newTodo,
+      todos,
+      toggleDone,
+      removeTodo,
+      markAllDone,
+      removeAll,
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +71,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+li {
+  font-size: 2rem;
+  color: black;
+}
+.todo {
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
+  color: silver;
 }
 </style>
